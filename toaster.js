7ced0b9957fa061f7dbb811aa101264082cafb25
Alex Soncodi <asoncodi@gmail.com>
2014-02-06 20:41:01 -0600

    Limit maximun number of visible toasts.

--- toaster.js
+++ toaster.js
@@ -1,18 +1,18 @@
 'use strict';
- 
+
 /*
  * AngularJS Toaster
  * Version: 0.4.1
  *
- * Copyright 2013 Jiri Kavulak.  
- * All Rights Reserved.  
- * Use, reproduction, distribution, and modification of this code is subject to the terms and 
+ * Copyright 2013 Jiri Kavulak.
+ * All Rights Reserved.
+ * Use, reproduction, distribution, and modification of this code is subject to the terms and
  * conditions of the MIT license, available at http://www.opensource.org/licenses/mit-license.php
  *
  * Author: Jiri Kavulak
  * Related to project of John Papa and Hans Fjällemark
  */
- 
+
 angular.module('toaster', ['ngAnimate'])
 .service('toaster', ['$rootScope', function ($rootScope) {
     this.pop = function (type, title, body, timeout, bodyOutputType) {
@@ -27,58 +27,59 @@ angular.module('toaster', ['ngAnimate'])
     };
 }])
 .constant('toasterConfig', {
-  				'tap-to-dismiss': true,
-					'newest-on-top': true,
-					//'fade-in': 1000,            // done in css
-					//'on-fade-in': undefined,    // not implemented
-					//'fade-out': 1000,           // done in css
-					// 'on-fade-out': undefined,  // not implemented
-					//'extended-time-out': 1000,    // not implemented
-					'time-out': 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
-					'icon-classes': {
-						error: 'toast-error',
-						info: 'toast-info',
-						success: 'toast-success',
-						warning: 'toast-warning'
-					},
-					'body-output-type': '', // Options: '', 'trustedHtml', 'template'
-					'body-template': 'toasterBodyTmpl.html',
-					'icon-class': 'toast-info',
-					'position-class': 'toast-top-right',
-					'title-class': 'toast-title',
-					'message-class': 'toast-message'
-				})
+          'limit': 0,
+          'tap-to-dismiss': true,
+          'newest-on-top': true,
+          //'fade-in': 1000,            // done in css
+          //'on-fade-in': undefined,    // not implemented
+          //'fade-out': 1000,           // done in css
+          // 'on-fade-out': undefined,  // not implemented
+          //'extended-time-out': 1000,    // not implemented
+          'time-out': 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
+          'icon-classes': {
+            error: 'toast-error',
+            info: 'toast-info',
+            success: 'toast-success',
+            warning: 'toast-warning'
+          },
+          'body-output-type': '', // Options: '', 'trustedHtml', 'template'
+          'body-template': 'toasterBodyTmpl.html',
+          'icon-class': 'toast-info',
+          'position-class': 'toast-top-right',
+          'title-class': 'toast-title',
+          'message-class': 'toast-message'
+        })
 .directive('toasterContainer', ['$compile', '$timeout', '$sce', 'toasterConfig', 'toaster',
 function ($compile, $timeout, $sce, toasterConfig, toaster) {
   return {
     replace: true,
     restrict: 'EA',
     link: function (scope, elm, attrs){
-      
+
       var id = 0;
-      
+
       var mergedConfig = toasterConfig;
       if (attrs.toasterOptions) {
           angular.extend(mergedConfig, scope.$eval(attrs.toasterOptions));
       }
-      
+
       scope.config = {
           position: mergedConfig['position-class'],
           title: mergedConfig['title-class'],
           message: mergedConfig['message-class'],
           tap: mergedConfig['tap-to-dismiss']
       };
-      
+
       function addToast (toast){
         toast.type = mergedConfig['icon-classes'][toast.type];
         if (!toast.type)
             toast.type = mergedConfig['icon-class'];
-        
+
         id++;
         angular.extend(toast, { id: id });
 
-	// Set the toast.bodyOutputType to the default if it isn't set
-	toast.bodyOutputType = toast.bodyOutputType || mergedConfig['body-output-type']
+  // Set the toast.bodyOutputType to the default if it isn't set
+  toast.bodyOutputType = toast.bodyOutputType || mergedConfig['body-output-type']
         switch(toast.bodyOutputType)
         {
           case 'trustedHtml':
@@ -88,35 +89,45 @@ function ($compile, $timeout, $sce, toasterConfig, toaster) {
             toast.bodyTemplate = mergedConfig['body-template'];
             break;
         }
-        
+
         var timeout = typeof(toast.timeout) == "number" ? toast.timeout : mergedConfig['time-out'];
         if (timeout > 0)
             setTimeout(toast, timeout);
-        
-        if (mergedConfig['newest-on-top'] === true)
+
+        if (mergedConfig['newest-on-top'] === true) {
             scope.toasters.unshift(toast);
-        else
+
+            if (mergedConfig['limit'] > 0 && scope.toasters.length > mergedConfig['limit']) {
+              scope.toasters.pop();
+            }
+        }
+        else {
             scope.toasters.push(toast);
+
+            if (mergedConfig['limit'] > 0 && scope.toasters.length > mergedConfig['limit']) {
+              scope.toasters.shift();
+            }
+        }
       }
-      
+
       function setTimeout(toast, time){
-          toast.timeout= $timeout(function (){ 
+          toast.timeout= $timeout(function (){
               scope.removeToast(toast.id);
             }, time);
       }
-      
+
       scope.toasters = [];
       scope.$on('toaster-newToast', function () {
         addToast(toaster.toast);
       });
     },
     controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-      
+
       $scope.stopTimer = function(toast){
         if(toast.timeout)
           $timeout.cancel(toast.timeout);
       };
-      
+
       $scope.removeToast = function (id){
         var i = 0;
         for (i; i < $scope.toasters.length; i++){
@@ -125,7 +136,7 @@ function ($compile, $timeout, $sce, toasterConfig, toaster) {
         }
         $scope.toasters.splice(i, 1);
       };
-      
+
       $scope.remove = function(id){
         if ($scope.config.tap === true){
             $scope.removeToast(id);
