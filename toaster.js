4597a9a52625337dac28dd6cef400c2e6e5dea3d
jirikavi <jirikavi@users.noreply.github.com>
2015-01-14 14:29:05 +0100

    Update toaster.js
    Adds option for calling methods like 'toaster.success', 'toaster.error', toaster.warning', toaster.info' and toaster.wait'.

--- toaster.js
+++ toaster.js
@@ -15,7 +15,33 @@
  */
 
 angular.module('toaster', ['ngAnimate'])
-.service('toaster', ['$rootScope', function ($rootScope) {
+.constant('toasterConfig', {
+    'limit': 0,                   // limits max number of toasts 
+    'tap-to-dismiss': true,
+    'close-button': false,
+    'newest-on-top': true,
+    //'fade-in': 1000,            // done in css
+    //'on-fade-in': undefined,    // not implemented
+    //'fade-out': 1000,           // done in css
+    // 'on-fade-out': undefined,  // not implemented
+    //'extended-time-out': 1000,    // not implemented
+    'time-out': 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
+    'icon-classes': {
+        error: 'toast-error',
+        info: 'toast-info',
+        wait: 'toast-wait',
+        success: 'toast-success',
+        warning: 'toast-warning'
+    },
+    'body-output-type': '', // Options: '', 'trustedHtml', 'template'
+    'body-template': 'toasterBodyTmpl.html',
+    'icon-class': 'toast-info',
+    'position-class': 'toast-top-right',
+    'title-class': 'toast-title',
+    'message-class': 'toast-message',
+    'mouseover-timer-stop': true // stop timeout on mouseover and restart timer on mouseout
+})
+.service('toaster', ['$rootScope', 'toasterConfig', function ($rootScope, toasterConfig) {
     this.pop = function (type, title, body, timeout, bodyOutputType, clickHandler) {
         if (angular.isObject(type)) {
             var params = type; // NOTE: anable parameters as pop argument
@@ -44,33 +70,18 @@ angular.module('toaster', ['ngAnimate'])
     this.clear = function () {
         $rootScope.$emit('toaster-clearToasts');
     };
+    
+    for (var type in toasterConfig['icon-classes']) {
+    this[type] = (function (toasterType){ 
+                    return function(title, body, timeout, bodyOutputType, clickHandler) {
+                                if (angular.isString(title))
+                                    this.pop(toasterType, title, body, timeout, bodyOutputType, clickHandler);
+                                else 
+                                    this.pop(angular.extend(title, {type: toasterType}));
+                               }
+                     })(type);
+    }
 }])
-.constant('toasterConfig', {
-    'limit': 0,                   // limits max number of toasts 
-    'tap-to-dismiss': true,
-    'close-button': false,
-    'newest-on-top': true,
-    //'fade-in': 1000,            // done in css
-    //'on-fade-in': undefined,    // not implemented
-    //'fade-out': 1000,           // done in css
-    // 'on-fade-out': undefined,  // not implemented
-    //'extended-time-out': 1000,    // not implemented
-    'time-out': 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
-    'icon-classes': {
-        error: 'toast-error',
-        info: 'toast-info',
-        wait: 'toast-wait',
-        success: 'toast-success',
-        warning: 'toast-warning'
-    },
-    'body-output-type': '', // Options: '', 'trustedHtml', 'template'
-    'body-template': 'toasterBodyTmpl.html',
-    'icon-class': 'toast-info',
-    'position-class': 'toast-top-right',
-    'title-class': 'toast-title',
-    'message-class': 'toast-message',
-    'mouseover-timer-stop': true // stop timeout on mouseover and restart timer on mouseout
-})
 .directive('toasterContainer', ['$compile', '$rootScope', '$interval', '$sce', 'toasterConfig', 'toaster',
 function ($compile, $rootScope, $interval, $sce, toasterConfig, toaster) {
     return {
