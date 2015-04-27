f47a0723a78ff91e6ae9dc552381f63c6e5d935a
Maxime Warnier <marmax@gmail.com>
2015-04-27 23:10:10 +0200

    Correction in comment
    extended timeout is not implemented and the timeout parameter is timeout instead of timeOut

--- toaster.js
+++ toaster.js
@@ -35,7 +35,7 @@ angular.module('toaster', ['ngAnimate'])
     //'fade-out': 1000,           // done in css
     //'on-fade-out': undefined,   // not implemented
     //'extended-time-out': 1000,  // not implemented
-    'time-out': 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
+    'time-out': 5000, // Set timeout to 0 to make it sticky
     'icon-classes': {
         error: 'toast-error',
         info: 'toast-info',
